948f327dcbf41870392fbe6d8df35874c29aba78
klmwook <klmwook72@gmail.com>
2023-06-12 13:01:37 +0900

    members의 아이디,비밀번호,이메일 밸리데이션 추가

--- js/form.js
+++ js/form.js
@@ -9,31 +9,63 @@ const form = document.querySelector('#member');
 const btnSubmit = form.querySelector('input[type=submit]');
 
 btnSubmit.addEventListener('click', (e) => {
-	if (!isTxt) e.preventDefault();
-	if (!isPwd) e.preventDefault();
-	if (!isEmail) e.preventDefault();
-	if (!isCheck) e.preventDefault();
-	if (!isSelect) e.preventDefault();
+	if (!isTxt('userid', 5)) e.preventDefault();
+	if (!isPwd('pwd1', 'pwd2', 5)) e.preventDefault();
+	if (!isEmail('email', 6)) e.preventDefault();
+	if (!isCheck()) e.preventDefault();
+	if (!isSelect()) e.preventDefault();
 });
 
-//userid 인증 함수
-function isTxt() {
+//텍스트항목 입력 받아 인증
+function isTxt(name, len) {
+	const input = form.querySelector(`[name=${name}]`);
+	const text = input.value.trim();
+	if (text.length < len) {
+		alert(`입력한 텍스트 항목을 ${len}글자 이상 입력 하세요.`);
+		return false;
+	} else {
+		return true;
+	}
+}
+
+//비밀번호 입력 받아 인증 함수
+//조건 - 다섯글자 이상 입력 , 2개의 비밀번호가 동일
+function isPwd(name, name2, len) {
+	const pwd = form.querySelector(`[name=${name}]`).value.trim();
+	const pwd2 = form.querySelector(`[name=${name2}]`).value.trim();
+
+	if (pwd !== pwd2 || pwd.length < len) {
+		alert(`비밀번호 항목 2개를 동일하게 입력하고 ${len}글자 이상 입력 하세요.`);
+		return false;
+	}
 	return true;
 }
 
-//pwd 인증 함수
-function isPwd() {
+//이메일 형식 입력 받아 인증 함수
+//조건 - 입력한 문자가 6글자 이상이고, @포함
+function isEmail(name, len) {
+	const input = form.querySelector(`input[name=${name}]`);
+	const Email = input.value.trim();
+
+	if (Email.length < len) {
+		alert(`입력한 이메일 항목을 ${len}글자 이상 입력 하세요.`);
+		return false;
+	}
+
+	if (Email.indexOf('@') === -1) {
+		alert(`@를 추가하여 이메일을 입력 해 주세요.`);
+		return false;
+	}
+
 	return true;
 }
 
-function isEmail() {
+//체크요소 형식 입력받아 인증
+function isCheck(name) {
 	return true;
 }
 
-function isCheck() {
-	return true;
-}
-
-function isSelect() {
+//select요소 입력 받아 인증
+function isSelect(name) {
 	return true;
 }
